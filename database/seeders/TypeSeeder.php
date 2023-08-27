04f89815dbd4c6b6b963742af82f0c505af11c55
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-08-28 01:59:59 +0200

    created typeSeeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $types = [
+            'Long term', 'Short term', 'Mid term'
+        ];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::of($type)->slug('-');
+            $newType->save();
+        }
+    }
+}
